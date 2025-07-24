7ec368965c4ea179e2b7519a9603ae00438907c4
Christoffer Ejemyr <christoffer.ejemyr@medmerabank.se>
2025-07-24 15:53:50 +0200

    Add logging

--- src/integrations/prefect-gcp/prefect_gcp/workers/cloud_run_v2.py
+++ src/integrations/prefect-gcp/prefect_gcp/workers/cloud_run_v2.py
@@ -209,6 +209,16 @@ class CloudRunWorkerJobV2Configuration(BaseJobConfiguration):
             work_pool: The work pool associated with the flow run used for preparation.
             worker_name: The worker name associated with the flow run used for preparation.
         """
+
+        self.flow_run_logger = flow_run_logger(flow_run=flow_run).getChild(
+            "worker",
+            extra={
+                "worker_name": worker_name,
+                "work_pool_name": work_pool.name if work_pool else "<unknown>",
+                "work_pool_id": str(work_pool.id if work_pool else "unknown"),
+            },
+        )
+
         super().prepare_for_flow_run(
             flow_run=flow_run,
             deployment=deployment,
@@ -344,6 +354,10 @@ class CloudRunWorkerJobV2Configuration(BaseJobConfiguration):
             context. (e.g. PREFECT_DEBUG_MODE, PREFECT__FLOW_RUN_ID, etc.)
         """
         # Itterate from back to keep the latest appended values for each env name
+        msg = f"Deduplicating environment variables: {self.job_body['template']['template']['containers'][0]['env']}"
+        print(msg)
+        self.flow_run_logger.info(msg)
+
         duplicate_envs = set()
         envs_to_keep = {}
         for env in reversed(
@@ -357,6 +371,10 @@ class CloudRunWorkerJobV2Configuration(BaseJobConfiguration):
         self.job_body["template"]["template"]["containers"][0]["env"] = list(
             envs_to_keep.values()
         )
+
+        msg = f"Deduplicated environment variables: {self.job_body['template']['template']['containers'][0]['env']}"
+        print(msg)
+        self.flow_run_logger.info(msg)
 
     def _configure_cloudsql_volumes(self):
         """
